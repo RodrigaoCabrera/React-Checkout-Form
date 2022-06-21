c8a29cea6879d4961a296cb5a8476cc390750438
Rodrigo Cabrera <rodrigod33d@gmail.com>
2022-06-21 01:02:08 -0300

    ADD: react-credit-cards, styled-components + estructura inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+
+import styled from "styled-components";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CardMain>
+      <Cards
+        number={"1234123412341234"}
+        name={"name"}
+        expiry={"2010"}
+        cvc={"123"}
+        focus={"name"}
+      />
+      <form>
+        <label>Full Name</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder='Your name'
+        />
+        <label>Card Number</label>
+        <input
+          type='number'
+          name='number'
+          id='number'
+          placeholder='1234 1234 1234 1234'
+        />
+        <label>Expiration</label>
+        <input
+          type='number'
+          name='expiry'
+          id='expiry'
+          placeholder='MM/YY'
+        />
+        <label>CVV</label>
+        <input
+          type='password'
+          name='cvc'
+          id='cvc'
+          placeholder='***'
+        />
+        <label>Zip Code</label>
+        <input
+          type='number'
+          name='zip-code'
+          id='zip-code'
+          placeholder='3615'
+        />
+
+        <div>
+          <button>Confirm Payment</button>
+          <span>You verify that this info is correct</span>
+        </div>
+      </form>
+    </CardMain>
   );
 }
 
-export default App;
+const CardMain = styled.div`
+  height: 100vh;
+  display: grid;
+  place-content: center;
+  background-color: #fbfbfb;
+`;
