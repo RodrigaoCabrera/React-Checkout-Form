2af268d585a6c1bc1bf6c15d73ff9642890d3dd1
Rodrigo Cabrera <rodrigod33d@gmail.com>
2022-06-22 20:16:09 -0300

    ADD: Reubicación y redimención de los inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useEffect } from "react";
 
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
@@ -16,48 +15,49 @@ export default function App() {
         cvc={"123"}
         focus={"name"}
       />
-      <form>
-        <label>Full Name</label>
-        <input
-          type='text'
-          name='name'
-          id='name'
-          placeholder='Your name'
-        />
-        <label>Card Number</label>
-        <input
-          type='number'
-          name='number'
-          id='number'
-          placeholder='1234 1234 1234 1234'
-        />
-        <label>Expiration</label>
-        <input
-          type='number'
-          name='expiry'
-          id='expiry'
-          placeholder='MM/YY'
-        />
-        <label>CVV</label>
-        <input
-          type='password'
-          name='cvc'
-          id='cvc'
-          placeholder='***'
-        />
-        <label>Zip Code</label>
-        <input
-          type='number'
-          name='zip-code'
-          id='zip-code'
-          placeholder='3615'
-        />
-
-        <div>
+      <Form>
+        <InputContainer>
+          <label>Full Name</label>
+          <input type="text" name="name" id="name" placeholder="Your name" />
+        </InputContainer>
+        <InputContainer>
+          <label>Card Number</label>
+          <input
+            type="number"
+            name="number"
+            id="number"
+            placeholder="1234 1234 1234 1234"
+          />
+        </InputContainer>
+        <ExpirationCvv>
+          <InputContainer>
+            <label>Expiration</label>
+            <input
+              type="number"
+              name="expiry"
+              id="expiry"
+              placeholder="MM/YY"
+            />
+          </InputContainer>
+          <InputContainer>
+            <label>CVV</label>
+            <input type="password" name="cvc" id="cvc" placeholder="***" />
+          </InputContainer>
+        </ExpirationCvv>
+        <InputContainer>
+          <label>Zip Code</label>
+          <input
+            type="number"
+            name="zip-code"
+            id="zip-code"
+            placeholder="3615"
+          />
+        </InputContainer>
+        <SubmitContainer>
           <button>Confirm Payment</button>
           <span>You verify that this info is correct</span>
-        </div>
-      </form>
+        </SubmitContainer>
+      </Form>
     </CardMain>
   );
 }
@@ -66,5 +66,36 @@ const CardMain = styled.div`
   height: 100vh;
   display: grid;
   place-content: center;
-  background-color: #fbfbfb;
+  background-color: #dadada;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: min(319px, 90%);
+  margin: 0 auto;
+  padding: 32px;
+  gap: 24px;
+  position: relative;
+  background: #ffffff;
+  border-radius: 8px;
+`;
+
+const InputContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ExpirationCvv = styled.div`
+  display: flex;
+  gap: 20px;
+  & input {
+    width: 100%;
+  }
+`;
+
+const SubmitContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
